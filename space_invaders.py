25e3afead199c79876b7f81f58103221038d4c8b
Emil Julian Costinea <ejc@spectra-systems.com>
2024-11-20 16:11:55 +0100

    add alien moving

--- space_invaders.py
+++ space_invaders.py
@@ -16,23 +16,86 @@ clock = pygame.time.Clock()
 
 
 class Game:
-    def __init__(self):
-        pass
+    def __init__(self, player, alien_group, player_bullet_group, alien_bullet_group):
+        self.round_number = 1
+        self.score = 0
+
+        self.player = player
+        self.alien_group = alien_group
+        self.player_bullet_group = player_bullet_group
+        self.alien_bullet_group = alien_bullet_group
+
+        self.new_round_sound = pygame.mixer.Sound("new_round.wav")
+        self.breach_sound = pygame.mixer.Sound("breach.wav")
+        self.alien_hit_sound = pygame.mixer.Sound("alien_hit.wav")
+        self.player_hit_sound = pygame.mixer.Sound("player_hit.wav")
+
+        self.font = pygame.font.Font('Facon.ttf', 32)
 
     def update(self):
-        pass
+        self.shift_aliens()
+        self.check_collision()
+        self.check_round_completion()
 
     def draw(self):
-        pass
+        WHITE = (255, 255, 255)
+
+        score_text = self.font.render(f"Score: {self.score}", True, WHITE)
+        score_rect = score_text.get_rect()
+        score_rect.centerx = WINDOW_WIDTH // 2
+        score_rect.top = 10
+
+        round_text = self.font.render(f"Round: {self.round_number}", True, WHITE)
+        round_rect = round_text.get_rect()
+        round_rect.topleft = (20, 10)
+
+        lives_text = self.font.render(f"Lives: {self.player.lives}", True, WHITE)
+        lives_rect = lives_text.get_rect()
+        lives_rect.topright = (WINDOW_WIDTH - 20, 10)
+
+        #Blit HUD
+        display_surface.blit(score_text, score_rect)
+        display_surface.blit(round_text, round_rect)
+        display_surface.blit(lives_text, lives_rect)
+
+        pygame.draw.line(display_surface, WHITE, (0, 50), (WINDOW_WIDTH, 50), 4)
+        pygame.draw.line(display_surface, WHITE, (0, WINDOW_HEIGHT - 100), (WINDOW_WIDTH, WINDOW_HEIGHT - 100), 4)
 
     def shift_aliens(self):
-        pass
+        shift = False
+        for alien in self.alien_group.sprites():
+            if alien.rect.right >= WINDOW_WIDTH or alien.rect.left <= 0:
+                shift = True
+                #maybe bug
+                break
+
+        if shift:
+            breach = False
+            for alien in self.alien_group.sprites():
+                alien.rect.y += 10 * self.round_number
+                alien.direction = -1 * alien.direction
+                alien.rect.x += alien.direction * alien.velocity
+
+                if alien.rect.bottom >= WINDOW_HEIGHT:
+                    breach = True
+
+            if breach:
+                self.breach_sound.play()
+                self.player.lives -= 1
+                self.check_game_status()
+
 
     def check_collision(self):
         pass
 
     def start_new_round(self):
-        pass
+        for i in range(11):
+            for j in range(5):
+                alien = Alien(64 + i * 64, 64 + j * 64, self.round_number, self.alien_bullet_group)
+                self.alien_group.add(alien)
+
+        self.new_round_sound.play()
+        self.pause_game()
 
     def check_round_completion(self):
         pass
@@ -168,11 +231,8 @@ my_player_group.add(my_player)
 
 my_alien_group = pygame.sprite.Group()
 
-for i in range(10):
-    alien = Alien(64 + i * 64, 100, 1, my_alien_bullet_group)
-    my_alien_group.add(alien)
-
 my_game = Game(my_player, my_alien_group, my_player_bullet_group, my_alien_bullet_group)
+my_game.start_new_round()
 
 # Main game loop
 running = True
